3f4733389e0c3416a33eadf55910d2bcf0a57e7b
newli <newri0807@naver.com>
2022-08-23 20:51:41 +0900

    Updates

---
+++ precache-manifest.38ac3b086f22612265a70418e27fcf25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c79904fb7a8938dda7dbfbefe6a5b8",
+    "url": "/reactStore/index.html"
+  },
+  {
+    "revision": "7ed771b985f8087c93d9",
+    "url": "/reactStore/static/css/2.40e60438.chunk.css"
+  },
+  {
+    "revision": "ea49ddced72c47a75ea2",
+    "url": "/reactStore/static/css/main.54bf843f.chunk.css"
+  },
+  {
+    "revision": "7ed771b985f8087c93d9",
+    "url": "/reactStore/static/js/2.5a5393f5.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/reactStore/static/js/2.5a5393f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea49ddced72c47a75ea2",
+    "url": "/reactStore/static/js/main.c981ab50.chunk.js"
+  },
+  {
+    "revision": "fc3cd02e12757b0f1fc2",
+    "url": "/reactStore/static/js/runtime-main.e6d83e78.js"
+  }
+]);
